ac1969afb3de93c460e7bfd2a5b031e513449a9a
David Doherty <froztydavie@Davids-MacBook-Air-5.local>
2025-05-01 14:14:13 -0700

    forms

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -3,23 +3,54 @@ from wtforms import StringField, DateField, SelectField, SubmitField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, URL
 
+def store_query():
+    from models import GroceryStore
+    return GroceryStore.query
+
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
-
-    # TODO: Add the following fields to the form class:
-    # - title - StringField
-    # - address - StringField
-    # - submit button
-    pass
+    title = StringField(
+        'Store Name',
+        validators=[DataRequired(), Length(max=100)]
+    )
+    address = StringField(
+        'Address',
+        validators=[DataRequired(), Length(max=200)]
+    )
+    submit = SubmitField('Submit')
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
 
-    # TODO: Add the following fields to the form class:
-    # - name - StringField
-    # - price - FloatField
-    # - category - SelectField (specify the 'choices' param)
-    # - photo_url - StringField
-    # - store - QuerySelectField (specify the `query_factory` param)
-    # - submit button
-    pass
+name = StringField(
+    'Item Name',
+    validators=[DataRequired(), Length(max=100)]
+)
+price = StringField(
+        'Price',
+        validators=[DataRequired()]
+    )
+category = SelectField(
+        'Category',
+        choices=[
+            ('Produce', 'Produce'),
+            ('Deli', 'Deli'),
+            ('Bakery', 'Bakery'),
+            ('Pantry', 'Pantry'),
+            ('Frozen', 'Frozen'),
+            ('Other', 'Other')
+        ],
+        validators=[DataRequired()]
+    )
+photo_url = StringField(
+        'Photo URL',
+        validators=[URL(), Length(max=200)]
+    )
+store_id = QuerySelectField(
+        'Store',
+        query_factory=lambda: GroceryStore.query.all(),
+        get_label='title',
+        allow_blank=True,
+        validators=[DataRequired()]
+    )
+submit = SubmitField('Submit')
