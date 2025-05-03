2056f52694f1eb5747c1dd23013b0d286f031c7e
David Doherty <froztydavie@Davids-MacBook-Air-5.local>
2025-05-02 23:07:48 -0700

    models updated

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -1,5 +1,23 @@
 from grocery_app.extensions import db
 from grocery_app.utils import FormEnum
+from flask_login import UserMixin
+from grocery_app.extensions import db
+
+shopping_list_table = db.Table(
+    'shopping_list',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('item_id', db.Integer, db.ForeignKey('grocery_item.id'))
+)
+
+class User(UserMixin, db.Model):
+    id = db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    shopping_list_items = db.relationship(
+        'GroceryItem',
+        secondary=shopping_list_table,
+        backref='user_who_want'
+    )
 
 
 class ItemCategory(FormEnum):
@@ -17,16 +35,18 @@ class GroceryStore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(200), nullable=False)
-    items = db.relationship('GroceryItem', back_populates='store')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    created_by = db.relationship('User')
+    # items = db.relationship('GroceryItem', back_populates='store')
 
 
 class GroceryItem(db.Model):
-    """Grocery Item model."""
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    price = db.Column(db.Float(precision=2), nullable=False)
-    category = db.Column(db.Enum(ItemCategory), default=ItemCategory.OTHER)
-    photo_url = db.Column(db.String)
-    store_id = db.Column(
-        db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
-    store = db.relationship('GroceryStore', back_populates='items')
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(20), nullable=False)
+    photo_url = db.Column(db.String(200))
+    store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'))
+    store = db.relationship('GroceryStore', backref='items')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
